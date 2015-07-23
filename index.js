3898eef1005c758531df0121a25084c9a156c057
Paul Frazee <pfrazee@gmail.com>
2015-07-23 16:23:06 -0500

    add sigil prefixes

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ function isString(s) {
 
 exports = module.exports =
   function (data) {
-    return isString(data) && /^[A-Za-z0-9\/+]{43}=\.[\w\d]+$/.test(data)
+    return isString(data) && /^(@|%|&)[A-Za-z0-9\/+]{43}=\.[\w\d]+$/.test(data)
   }
 
 exports.isHash =
   function (data) {
-    return isString(data) && /^[A-Za-z0-9\/+]{43}=\.sha256$/.test(data)
+    return isString(data) && /^(@|%|&)[A-Za-z0-9\/+]{43}=\.sha256$/.test(data)
   }
 
 exports.isFeedId =
   function (data) {
-    return isString(data) && /^[A-Za-z0-9\/+]{43}=\.(?:sha256|ed25519)$/.test(data)
+    return isString(data) && /^@[A-Za-z0-9\/+]{43}=\.(?:sha256|ed25519)$/.test(data)
+  }
+
+exports.isMsgId = 
+  function (data) {
+    return isString(data) && /^%[A-Za-z0-9\/+]{43}=\.sha256$/.test(data)
+  }
+
+exports.isBlobId = 
+  function (data) {
+    return isString(data) && /^&[A-Za-z0-9\/+]{43}=\.sha256$/.test(data)
   }
 
 exports.isRef = exports
