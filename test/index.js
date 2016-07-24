81fe5a5466c25013cb5084f5a9be4e2a23169300
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-24 23:25:22 +1200

    test local ipv6 addresses

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var ipv6Addr = "2a03:2267::ba27:ebff:fe8c:5a4d:8080:@gYCJpN4eGDjHFnWW2Fcusj8O4QYbVDUW6rNYh7nNEnc=.ed25519"
 var ipv6Invite = "2a03:2267::ba27:ebff:fe8c:5a4d:8080:@gYCJpN4eGDjHFnWW2Fcusj8O4QYbVDUW6rNYh7nNEnc=.ed25519~DxiHEv+ds+zUzA49efDgZk8ssGeqrp/5kgvRVzTM7vU="
+var ipv6AddrLocal = "::1:8080:@gYCJpN4eGDjHFnWW2Fcusj8O4QYbVDUW6rNYh7nNEnc=.ed25519"
+var ipv6InviteLocal = "::1:8080:@gYCJpN4eGDjHFnWW2Fcusj8O4QYbVDUW6rNYh7nNEnc=.ed25519~DxiHEv+ds+zUzA49efDgZk8ssGeqrp/5kgvRVzTM7vU="
 
 var R = require('../')
 var tape = require('tape')
@@ -7,6 +9,8 @@ var tape = require('tape')
 tape('ipv6 invite', function (t) {
   t.ok(R.isAddress(ipv6Addr))
   t.ok(R.isInvite(ipv6Invite))
+  t.ok(R.isAddress(ipv6AddrLocal))
+  t.ok(R.isInvite(ipv6InviteLocal))
   t.end()
 })
 
@@ -51,3 +55,5 @@ tape('extract', function (t) {
   })
   t.end()
 })
+
+
