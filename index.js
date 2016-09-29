06e4114b8e2b3eae60b77cd00c266f5391bb4b87
Dominic Tarr <dominic.tarr@gmail.com>
2016-09-30 12:00:19 +1300

    implement invite parsing stuff

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function isIP (s) {
 }
 
 var isInteger = Number.isInteger
+var DEFAULT_PORT = 8008
 
 function isString(s) {
   return 'string' === typeof s
@@ -42,13 +43,104 @@ var isAddress = exports.isAddress =
   )
 }
 
-var isInvite = exports.isInvite =
+var parseAddress = exports.parseAddress = function (e) {
+  if(isString(e)) {
+    var parts = e.split(':')
+    var id = parts.pop(), port = parts.pop(), host = parts.join(':')
+    var e = {
+      host: host,
+      port: +(port || DEFAULT_PORT),
+      key: id
+    }
+    return e
+  }
+  return e
+}
+
+var toAddress = exports.toAddress = function (e) {
+  e = exports.parseAddress(e)
+  e.port = e.port || DEFAULT_PORT
+  e.host = e.host || 'localhost'
+  return e
+}
+
+
+var isLegacyInvite = exports.isLegacyInvite =
   function (data) {
     if(!isString(data)) return false
     var parts = data.split('~')
-    //console.log(parts, isAddress(parts[0]), /^[A-Za-z0-9\/+]{43}=$/.test(parts[1]))
+    console.log('isAddress?', parts[0], isAddress(parts[0]))
     return parts.length == 2 && isAddress(parts[0]) && /^[A-Za-z0-9\/+]{43}=$/.test(parts[1])
   }
+
+var isMultiServerInvite = exports.isMultiServerInvite =
+  function (data) {
+    if(!isString(data)) return false
+    return !!parseMultiServerInvite(data)
+  }
+
+var isInvite = exports.isInvite =
+  function (data) {
+    console.log('isInvite?', data, isLegacyInvite(data), isMultiServerInvite(data))
+    if(!isString(data)) return false
+    return isLegacyInvite(data) || isMultiServerInvite(data)
+  }
+
+
+function parseLegacyInvite (invite) {
+  var redirect = invite.split('#')
+  invite = redirect.shift()
+  var parts = invite.split('~')
+  var addr = toAddress(parts[0])//.split(':')
+  //convert legacy code to multiserver invite code.
+  var remote = 'net:'+addr.host+':'+addr.port+'~shs:'+addr.key.slice(1, -8)
+  invite = remote+':'+parts[1]
+  return {
+    invite: remote + ':' + parts[1],
+    key: addr.key,
+    redirect: null,
+    remote: remote,
+    redirect: redirect.length ? '#' + redirect.join('#') : null
+  }
+}
+
+function parseMultiServerInvite (invite) {
+
+  var redirect = invite.split('#')
+  if(!redirect.length) return null
+
+  invite = redirect.shift()
+
+  var parts = invite.split('~')
+  .map(function (e) { return e.split(':') })
+
+  if(parts.length !== 2) return null
+  if(!/^(net|wss?)$/.test(parts[0][0])) return null
+  if(parts[1][0] !== 'shs') return null
+  if(parts[1].length !== 3) return null
+  var p2 = invite.split(':')
+  p2.pop()
+
+  return {
+    invite: invite,
+    remote: p2.join(':'),
+    key: '@'+parts[1][1]+'.ed25519',
+    redirect: redirect.length ? '#' + redirect.join('#') : null
+  }
+}
+
+exports.parseLegacyInvite = parseLegacyInvite
+exports.parseMultiServerInvite = parseMultiServerInvite
+
+exports.parseInvite = function (invite) {
+  return (
+    isLegacyInvite(invite)
+  ? parseLegacyInvite(invite)
+  : isMultiServerInvite(invite)
+  ? parseMultiServerInvite(invite)
+  : null
+  )
+}
 
 exports.type =
   function (id) {
@@ -79,5 +171,4 @@ exports.extract =
     var res = /([@%&][A-Za-z0-9\/+]{43}=\.[\w\d]+)/.exec(_data)
     return res && res[0]
   }
-
 
